9d25943eb8520e05da152ef3f9b26ccc3506ee50
Joe <jhhong0930@gmail.com>
2021-08-21 20:29:58 +0900

    연관관계 매핑

--- workspace/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
+++ workspace/ex1hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -18,14 +18,21 @@ public class JpaMain {
         tx.begin();
 
         try {
-//            Member findMember = em.find(Member.class, 1L);
-            List<Member> result = em.createQuery("select m from Member as m", Member.class)
-                    .setFirstResult(1)
-                    .setMaxResults(10)
-                    .getResultList();
 
-            for (Member member : result) {
-                System.out.println("member.name = " + member.getName());
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.changeTeam(team);
+            em.persist(member);
+
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
             }
 
             tx.commit();
