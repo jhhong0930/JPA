ae22a88524e456f55f9dc58a61885ac949ea72fb
jhhong0930 <jhhong0930@gmail.com>
2021-09-22 12:54:55 +0900

    순수 JPA 기반 리포지토리 만들기

--- workspace/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ workspace/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -5,6 +5,8 @@ import study.datajpa.entity.Member;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public class MemberJpaRepository {
@@ -17,8 +19,27 @@ public class MemberJpaRepository {
         return member;
     }
 
+    // 전체 조회
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    // 단건 조회
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
+    }
+
     public Member find(Long id) {
         return em.find(Member.class, id);
+    }
+
+    public void delete(Member member) {
+        em.remove(member);
     }
 
 }
