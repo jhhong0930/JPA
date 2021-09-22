a65222f0f70f5234fa123e3232c5e5799d608ca8
jhhong0930 <jhhong0930@gmail.com>
2021-09-22 11:19:16 +0900

    스프링 데이터 JPA와 DB 설정, 동작 확인

---
+++ workspace/data-jpa/src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,30 @@
+package study.datajpa.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    protected Member() {
+
+    }
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+}
