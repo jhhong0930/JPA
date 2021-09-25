f6f5b2da6c7d5b83b8ca366473a9a423305750a0
jhhong0930 <jhhong0930@gmail.com>
2021-09-25 15:51:45 +0900

    엔티티 클래스 설계

---
+++ workspace/jpashop/src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Delivery {
+
+    @Id @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status;
+    // EnumType.ORDINAL 은 숫자로 들어가게 되는데
+    // 이넘 타입이 추가되면 숫자가 밀리게 되어 정보들이 섞인다
+    // 따라서 문자열로 해주어야 안전하다
+
+}
