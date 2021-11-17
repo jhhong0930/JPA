fd63e95ad3a13cd2bcbd074b0eaab43b594340a1
jhhong0930 <jhhong0930@gmail.com>
2021-11-18 00:16:59 +0900

    검색 조건 쿼리

--- workspace/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
+++ workspace/querydsl/src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -73,4 +73,37 @@ public class QuerydslBasicTest {
 
         assertThat(findMember.getUsername()).isEqualTo("member1");
     }
+
+    @Test
+    void search() {
+
+        Member findMember = queryFactory
+                .selectFrom(member)
+//                .where(member.username.eq("member1")
+//                        .and(member.age.eq(10)))
+                .where(
+                        member.username.eq("member1"),
+                        member.age.eq(10)
+                )
+                .fetchOne();
+
+        /*
+        member.username.eq("member1")        // username = 'member1'
+        member.username.ne("member1")        // username != 'member1'
+        member.username.eq("member1").not()  // username != 'member1'
+        member.username.isNotNull()          // 이름이 is not null
+        member.age.in(10, 20)                // age in (10,20)
+        member.age.notIn(10, 20)             // age not in (10, 20)
+        member.age.between(10,30)            // between 10, 30
+        member.age.goe(30)                   // age >= 30
+        member.age.gt(30)                    // age > 30
+        member.age.loe(30)                   // age <= 30
+        member.age.lt(30)                    // age < 30
+        member.username.like("member%")      // like 검색
+        member.username.contains("member")   // like ‘%member%’ 검색
+        member.username.startsWith("member") // like ‘member%’ 검색
+         */
+
+        assertThat(findMember.getUsername()).isEqualTo("member1");
+    }
 }
